2612eb2bfbdc85971126859722df010f14cc2064
Deividi-Lucas <deividibaby6@gmail.com>
2023-04-16 16:33:58 -0400

    Chore: new table colors add

---
+++ src/utils/color.ts
@@ -0,0 +1,10 @@
+export const color ={
+    'green': 'rgb(0,255,0)',
+    'red': 'rgb(255,0,0)',
+    'yellow': 'rgb(255,255,0)',
+    'rosa': 'rgb(255,0,255)',
+    'ciano': 'rgb(0,255,255)',
+    'branco': 'rgb(255,255,255)',
+    'preto': 'rgb(0,0,0)'
+
+}
