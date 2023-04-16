20f0f783b82c950d9a6b9d89d45d5e654fe11914
Deividi-Lucas <deividibaby6@gmail.com>
2023-04-16 16:49:21 -0400

    Chore: Bug in folder

---
+++ src/components/Post/style.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Botao = styled.button`
+height: min-content;
+padding: 1rem;
+`
+
+
+export const Writes = styled.input`
+overflow-x: hidden;
+
+
+`
+
+export const Write = styled.textarea`
+resize: none;
+
+`
+
+
